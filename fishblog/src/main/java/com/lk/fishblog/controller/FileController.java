83a2e73ff7c1403bb9c0dbc0d92ea81ba54bca2d
liuk123 <liu___k@outlook.com>
2020-09-22 09:59:33 +0800

    上传文件修改

--- fishblog/src/main/java/com/lk/fishblog/controller/FileController.java
+++ fishblog/src/main/java/com/lk/fishblog/controller/FileController.java
@@ -13,6 +13,8 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.UUID;
 
 @RestController
@@ -24,27 +26,36 @@ public class FileController {
     @Autowired
     ArticleService articleService;
 
+    public final static String UPLOAD_PATH_PREFIX = "uploadFile/";
+
     @PostMapping(path = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public ResultSet addByJson(@RequestParam(value = "uploadFile") MultipartFile file, HttpServletRequest request){
         if(file.isEmpty()){
             return new ResultSet(ResultSet.RESULT_CODE_FALSE, "文件不可为空");
         }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd/");
+        String realPath = "../" + UPLOAD_PATH_PREFIX;
+        String format = sdf.format(new Date());
+        //存放上传文件的文件夹
+        File realFile = new File(realPath + format);
+
         String fileName = file.getOriginalFilename();
         String fileSuffix = fileName.substring(fileName.lastIndexOf("."));
         fileName = UUID.randomUUID() + fileSuffix;
-        String filePath = "D://temp-rainy//";
-        File dest = new File(filePath + fileName);
+        File newFile = new File(realFile.getAbsolutePath() + File.separator + fileName);
 
-        if(!dest.getParentFile().exists()){
-            dest.getParentFile().mkdir();
+        if(!newFile.isDirectory()){
+            newFile.mkdirs();
         }
         try{
-            file.transferTo(dest);
+            file.transferTo(newFile);
+            String filePath = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/uploadFile/" + format + fileName;
+            return new ResultSet(ResultSet.RESULT_CODE_TRUE, "上传成功", filePath);
         }catch (IOException e){
             e.printStackTrace();
         }
-        String path = "/temp-rainy/" + fileName;
-        return new ResultSet(ResultSet.RESULT_CODE_TRUE, "上传成功", path);
+
+        return new ResultSet(ResultSet.RESULT_CODE_FALSE, "上传失败");
     }
 }
